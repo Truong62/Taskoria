12f17af0938e927d8b3be41942aeb248cfba51ae
NNT <121045699+Truong62@users.noreply.github.com>
2025-08-01 17:51:47 +0700

    Update config.go

--- auth-service/config/config.go
+++ auth-service/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
@@ -14,6 +15,8 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() *gorm.DB {
+	createDatabaseIfNotExists()
+
 	dsn := os.Getenv("DB_URL")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -28,4 +31,49 @@ func ConnectDB() *gorm.DB {
 	DB = db
 	fmt.Println("✅ Connected to DB successfully")
 	return db
+}
+
+func createDatabaseIfNotExists() {
+	// Connect to Postgres
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER_AUTH"),
+		os.Getenv("DB_PASSWORD_AUTH"),
+		os.Getenv("DB_PORT_AUTH"),
+	)
+
+	drivenName := "postgres"
+	db, err := sql.Open(drivenName, dsn)
+	if err != nil {
+		log.Fatalf("Failed to connect to PostgreSQL server: %v", err)
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Printf("Error closing database: %v", err)
+		}
+	}(db)
+
+	dbName := os.Getenv("DB_NAME_AUTH")
+
+	// Check if the database exists
+	var exists bool
+	err = db.QueryRow("SELECT EXISTS(SELECT 1 FROM pg_database WHERE datname=$1)", dbName).Scan(&exists)
+	if err != nil {
+		log.Fatalf("Failed to check if database exists: %v", err)
+	}
+
+	if exists {
+		log.Printf("Database already exists: %s", dbName)
+		return
+	}
+
+	// Create database if not found
+	_, err = db.Exec("CREATE DATABASE " + dbName)
+	if err != nil {
+		log.Printf("Error creating database: %v", err)
+	} else {
+		log.Println("Database created:", dbName)
+	}
 }
